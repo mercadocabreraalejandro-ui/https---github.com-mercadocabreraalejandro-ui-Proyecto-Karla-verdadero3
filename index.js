70d56f588065c5dc280de3b608abf81ae88fd8fc
Alejandro Mercado <mercadocabreraalejandro@gmail.com>
2026-03-20 19:42:03 -0400

    Proyecto 1

---
+++ index.js
@@ -0,0 +1,6 @@
+let encuesta = window.confirm("¿Do you want to rate my brochure")
+let nombre = encuesta ? window.prompt("What's your name?") :  window.location.href = "https://www.canva.com/design/DAHEh3pcSgk/APhHeKFFdVBCsgD8UL4FJg/edit?ui=e30&referrer=https%3A%2F%2Fwww.canva.com%2Ftemplates";
+  window.prompt("From 1-10 how much do you give my brochure?") 
+
+window.alert(`Thank you ${nombre} for rate my brochure `)
+window.location.href = "https://www.canva.com/design/DAHEh3pcSgk/APhHeKFFdVBCsgD8UL4FJg/edit?ui=e30&referrer=https%3A%2F%2Fwww.canva.com%2Ftemplates";
